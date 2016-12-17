f833eadaec3747928f94cc6d7c4c679fabae6893
Exitialis <lpexitialis@gmail.com>
2016-12-17 03:17:13 +0300

    Полный отказ от ts

--- app/app.js
+++ app/app.js
@@ -1,14 +1,18 @@
-"use strict";
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var api = require('../routes/api');
-var app = express();
+import * as express from 'express';
+import * as path from 'path';
+import * as favicon from 'serve-favicon';
+import * as logger from 'morgan';
+import * as bodyParser from 'body-parser';
+import * as cookieParser from 'cookie-parser';
+
+import * as api from '../routes/api';
+
+let app: any = express();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -22,20 +26,26 @@ app.use(cookieParser());
     sourceMap: true
 }));*/
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/', api);
+
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
+app.use(function(req, res, next) {
+    let err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
+
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
+
     // render the error page
     res.status(err.status || 500);
     res.render('error');
 });
+
 module.exports = app;
+
